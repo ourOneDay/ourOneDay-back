8b364fcdbaa5126e50a0686ce0899c77acf25a5f
bbungjin <leejintwo12@naver.com>
2025-03-31 01:43:17 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/ourOneDay/ourOneDay/OurOneDayApplication.java
@@ -0,0 +1,13 @@
+package ourOneDay.ourOneDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurOneDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurOneDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ourOneDay/ourOneDay/OurOneDayApplicationTests.java
@@ -0,0 +1,13 @@
+package ourOneDay.ourOneDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurOneDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
